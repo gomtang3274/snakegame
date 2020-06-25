b0ddf45810f4f86e313dc90f2f7aad98a140d0b1
gomtang3274 <52527817+gomtang3274@users.noreply.github.com>
2020-06-25 19:05:40 +0900

    Update snakecopy.cpp

--- snakecopy.cpp
+++ snakecopy.cpp
@@ -18,6 +18,7 @@ snakegame::snakegame()
     noecho();
     curs_set(0);
     getmaxyx(stdscr, maxheight, maxwidth);
+    maxwidth -= 20;
     //초기화
     partchar='x';
     oldalchar=(char)219;
@@ -29,6 +30,10 @@ snakegame::snakegame()
     growthItem.y=0;
     poisonItem.x=0;
     poisonItem.y=0;
+    snakeLength = 5;
+    usingGate = 0;
+    eatFood = 0;
+    eatPoison = 0;
 
     for(int i =0;i<5;i++)
         snake.push_back(partpos(40+i,10));
@@ -64,7 +69,7 @@ snakegame::snakegame()
         addch(partchar);
     }
     move(maxheight-1,0);
-    printw("%d",points);
+    showpoint();
     move(growthItem.y,growthItem.x);
     addch(foo);
     refresh();
@@ -150,6 +155,25 @@ void snakegame::putItem()
     }
 }
 
+void snakegame::showpoint()
+{
+    move(2,maxwidth-1);
+    printw("   YOUR SCORE");
+    move(3,maxwidth-1);
+    printw("   -----------");
+    move(4,maxwidth-1);
+    printw("   Point: \t %d",points);
+    move(5,maxwidth-1);
+    printw("   Size: \t %d",snakeLength);
+    move(6,maxwidth-1);
+    printw("   O: \t %d",eatFood);
+    move(7,maxwidth-1);
+    printw("   @: \t %d",eatPoison);
+    move(8,maxwidth-1);
+    printw("   Gate: \t %d",usingGate);
+
+}
+
 bool snakegame::collision()
 {
     if(snake[0].x==0 || snake[0].x==maxwidth-1||snake[0].y==0||snake[0].y==maxheight-2)
@@ -162,18 +186,22 @@ bool snakegame::collision()
     if(snake[0].x==growthItem.x&&snake[0].y==growthItem.y){
         checked=1;
         putItem();
-        points+=10;
+        points+=15;
+        snakeLength++;
+        eatFood++;
         move(maxheight-1,0);
-        printw("%d",points);
+        showpoint();
         if((points%100)==0)
             del-=10000;
     }
     else if(snake[0].x==poisonItem.x&&snake[0].y==poisonItem.y){
         checked=2;
         putItem();
-        points+=15;
+        points+=10;
+        snakeLength--;
+        eatPoison++;
         move(maxheight-1,0);
-        printw("%d",points);
+        showpoint();
         if((points%100)==0)
             del-=10000;
         if(snake.size() <= 3){
@@ -266,7 +294,7 @@ void snakegame::start()
     {
         if(collision())
         {
-            move(12,36);
+            move(maxheight/2,maxwidth/2);
             printw("game over");
             break;
         }
